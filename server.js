88d297fdcc60634f19bf8e5b875bcdd3677bc8ad
Carlos Asencio Morales <carlosgkt@gmail.com>
2024-04-03 18:59:30 -0500

    initial config for server.js

---
+++ server.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handlebars setup
+const hbs = exphbs.create({});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Session middleware for authentication
+app.use(session({
+  secret: 'password',
+  resave: false,
+  saveUninitialized: true,
+}));
+
+// Routes
+app.use(require('./controllers/'));
+
+// Database synchronization
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
+});
