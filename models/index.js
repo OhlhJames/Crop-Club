4a92a13a7e71e3079badafcc444d4e2d712ea83d
Benjamin Brechter <benbrechter3@gmail.com>
2024-04-04 20:25:32 -0500

    I put in the code for the models/index.js

--- models/index.js
+++ models/index.js
@@ -3,21 +3,29 @@ const Farmer = require('./farmer');
 const Reviews = require('./Review');
 const Produce = require('./produce');
 
-// Farmer.belongsToMany(Produce, {
-//     foreignKey: 'farmerId'
-// });
- 
-// Produce.belongsTo(Farmer, {
-//     foreignKey: 'farmerId'
-// });
-
-// Produce.belongsToMany(Reviews, {
-//     foreignKey: 'produceId'
-// });
-
-// Reviews.belongsTo(Produce, {
-//     foreignKey: 'produceId'
-// });
-
-
+User.hasOne(Farmer, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+  });
+  Farmer.belongsTo(User, {
+    foreignKey: 'userId',
+  });
+  // Defining that a Farmer can have many Produces
+  Farmer.hasMany(Produce, {
+    foreignKey: 'farmerId',
+    onDelete: 'CASCADE',
+  });
+  // Each Produce is associated with a Farmer
+  Produce.belongsTo(Farmer, {
+    foreignKey: 'farmerId',
+  });
+  // A User can have many Reviews
+  User.hasMany(Reviews, {
+    foreignKey: 'userId',
+    onDelete: 'CASCADE',
+  });
+  // Each Review is written by a User
+  Reviews.belongsTo(User, {
+    foreignKey: 'userId',
+  });
 module.exports = {User, Farmer, Reviews, Produce}
