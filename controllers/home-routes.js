b38d0cd22fe3ad6a843cc657cadd149cad55f1f9
britney padron <brit@britneysmacair.attlocal.net>
2024-04-08 19:36:57 -0500

    Route get produce by id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -97,3 +97,15 @@ router.get('/reviews/id' ,async (req, res) => {
     })
 
     module.exports = router
+
+    router.get('/produce/:id' ,async (req, res) => {
+      try{
+       const ProduceData = await Produce.findByPk(req.params.Id, { 
+         attributes:{}
+         });
+         res.status(200).json(ProduceData);
+      }catch (err){
+      res.status(500).json(err);
+      }
+   });
+   
